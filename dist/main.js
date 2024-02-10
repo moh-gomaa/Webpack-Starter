3546c680c0e4683d92cc894cb831a1e1509c51e1
Mohamed Gomaa <mohamed.adel36@msa.edu.eg>
2024-02-10 09:14:29 +0200

    -Project initialization.
    -Add main core of the project.

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("What did the horse say after it tripped? Help! I’ve fallen and I can’t giddyup!")})();
---
+++ src/generateJoke.js
@@ -0,0 +1,6 @@
+
+function generateJoke(){
+    return 'What did the horse say after it tripped? Help! I’ve fallen and I can’t giddyup!';
+}
+
+export default generateJoke;
